05e6ebb0e3b46a86c31b706484bfcc36897a951f
unknown <Administrator@KANINI-LTP-485.gisbizdev.gisbiz.chennai.com>
2021-12-06 10:25:51 +0530

    Initial commit

---
+++ app/src/main/java/com/example/task4/MainActivity.kt
@@ -0,0 +1,57 @@
+package com.example.task4
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import com.google.android.material.textfield.TextInputLayout
+import kotlin.random.Random
+
+class MainActivity : AppCompatActivity() {
+    var attempt = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var randNumber = Random.nextInt(0 , 1000)
+        val guessMsg = findViewById<TextView>(R.id.AnswerText)
+        val stillMsg = findViewById<TextView>(R.id.FoundSol)
+
+        findViewById<TextView>(R.id.GuessButton).setOnClickListener {
+            val guessInput = findViewById<TextInputLayout>(R.id.InputNumber)
+            val guessNumber = guessInput.editText?.text?.toString()?.toInt()
+            when {
+                attempt <= 12 && guessNumber == 0 -> {
+                    guessMsg.text = "Enter A Valid Number"
+                    stillMsg.text = "you haven't yet found the solution"
+                    attempt++
+                }
+                attempt <= 12 && randNumber > guessNumber!! -> {
+                    guessMsg.text = "no :( My Number Is Bigger"
+                    stillMsg.text = "you haven't yet found the solution"
+                    attempt++
+                }
+                attempt <= 12 && randNumber < guessNumber!! -> {
+                    guessMsg.text = "no :( My Number Is Smaller"
+                    stillMsg.text = "you haven't yet found the solution"
+                    attempt++
+                }
+                attempt <= 12 && randNumber == guessNumber!! -> {
+                    val newScreenIntent = Intent(this, Activity_end::class.java)
+                    newScreenIntent.putExtra("Number" , "$randNumber" )
+                    startActivity(newScreenIntent)
+                    randNumber = Random.nextInt(0 , 1000)
+                    attempt = 0
+                }
+                attempt > 12 -> {
+                    val newScreenIntent = Intent(this, Activity_end::class.java)
+                    newScreenIntent.putExtra("Number" , "$randNumber" )
+                    startActivity(newScreenIntent)
+                    randNumber = Random.nextInt(0 , 1000)
+                    attempt = 0
+                }
+            }
+        }
+    }
+}
